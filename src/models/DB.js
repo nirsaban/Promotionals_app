a3d9197f9144848584cca391ccaa7e9dc8f2c18a
Nir <59836189+nirsaban@users.noreply.github.com>
2021-10-03 10:56:29 +0300

    connect

--- src/models/DB.js
+++ src/models/DB.js
@@ -1,13 +1,17 @@
 const mysql = require('mysql');
 let db = mysql.createConnection({
     host     : 'https://academyjob.org',
+    port:'3306',
     user     : 'onclickj_naor',
     password : 'parnasa2020',
     database:"onclickj_naor"
 });
 
 db.connect((err) => {
-    if (err) throw err;
+    if (err) {
+        console.log(err)
+        throw err
+    };
 });
 
 module.exports = db;
