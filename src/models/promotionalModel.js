22fcf36c01b090857e2b179ae9f038b27790db36
Nir <59836189+nirsaban@users.noreply.github.com>
2021-10-03 21:20:30 +0300

    connect

--- src/models/promotionalModel.js
+++ src/models/promotionalModel.js
@@ -1,17 +1,17 @@
-var sql = require('./db.js');
+// var sql = require('./db.js');
 
 const  Promotional = {};
 
 
 Promotional.addPromotional = ({state:{product_name,more_info,product_price,provider_name}}, result) => {  
-    sql.query("INSERT INTO `promotionals` VALUES(NULL,?,?,?,?,NOW())", [product_name,provider_name,more_info,product_price],  (err, res) => {  
-       if(err) {
-           result(err, null);
-       }
-       else{
-           result(null, res);
-       }
-   });           
+//     sql.query("INSERT INTO `promotionals` VALUES(NULL,?,?,?,?,NOW())", [product_name,provider_name,more_info,product_price],  (err, res) => {  
+//        if(err) {
+//            result(err, null);
+//        }
+//        else{
+//            result(null, res);
+//        }
+//    });           
 }
 
 module.exports = Promotional;
