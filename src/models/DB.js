64cac2c07d1f4633f26f550b4c4cc047c742f090
Nir <59836189+nirsaban@users.noreply.github.com>
2021-10-03 21:07:31 +0300

    connect

--- src/models/DB.js
+++ src/models/DB.js
@@ -1,16 +1,17 @@
-// const mysql = require('mysql');
-// let db = mysql.createConnection({
-//     host     : 'localhost',
-//     user     : 'root',
-//     password : '',
-//     database:"naor"
-// });
+const mysql = require('mysql');
+let db = mysql.createConnection({
+    host     : 'academyjob.org',
+    user     : 'onclickj_naor',
+    port:'3306',
+    password : 'parnasa2020S',
+    database:"onclickj_naor"
+});
 
-// db.connect((err) => {
-//     if (err) {
-//         console.log(err)
-//         throw err
-//     };
-// });
+db.connect((err) => {
+    if (err) {
+        console.log(err)
+        throw err
+    };
+});
 
-// module.exports = db;
+module.exports = db;
