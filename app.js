fb76963e5291b0bfaab42edb0690c10bbc1b080e
Nir <59836189+nirsaban@users.noreply.github.com>
2021-10-03 08:50:23 +0300

    secondjj commit

--- app.js
+++ app.js
@@ -6,17 +6,15 @@ const routes = require('./src/routes/apiRoutes'); //importing route
 const path = require('path');
 
 const app = express();
+
 app.use(helmet());
 app.use(express.json());
 app.use(cors())
 app.use(morgan('combined'));
-
+app.use(express.static(path.join(__dirname, 'build_copy')));
 
 routes(app);
-app.use(express.static(path.join(__dirname, 'build_copy')));
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build_copy', 'index.html'));
-  });
+
   
 app.listen(process.env.PORT || 3000, function(){
    
--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -1,3 +1,4 @@
+const path = require('path');
 module.exports = (app) => {
 
 const provider = require('../controllers/providersControllers.js');
@@ -6,5 +7,8 @@ app.route('/add_provider').post(provider.add_provider)
 app.route('/get_providers').get(provider.get_providers)
 app.route('/add_product').post(promotionals.add_promotional)
 
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build_copy', 'index.html'));
+  });
 
 }
