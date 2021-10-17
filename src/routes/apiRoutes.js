41ad5b408778d7dae15736aa84c1c929686a5da6
Nir <59836189+nirsaban@users.noreply.github.com>
2021-10-17 19:13:29 +0300

    responsive table

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -6,6 +6,7 @@ const promotionals = require('../controllers/promotionalControllers.js');
 app.route('/add_provider').post(provider.add_provider)
 app.route('/get_providers').get(provider.get_providers)
 app.route('/get_last_month').get(promotionals.get_last_month)
+
 app.route('/add_product').post(promotionals.add_promotional)
 
 
